710742b2fe7227a7025bfc61e73f88e8e1d6e9fb
oleg voronin <oleg83voronin@gmail.com>
2024-10-23 21:08:54 +0300

    change file

--- Family_Tree/People.java
+++ Family_Tree/People.java
@@ -1,16 +1,54 @@
 package Family_Tree;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
+
+import java.util.List;
 
 public class People {
 
-    String name;
-    LocalDate birthDate;
-    String mather;
-    String father;
-    ArrayList<String> children;
+   private String name;
+   private LocalDate birthDate;
+   private People mather;
+   private People father;
+   private List<People> children;
 
-    public 
+    public People(String name, LocalDate birthDate) {
+
+        this.name = name;
+        this.birthDate = birthDate;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setMather(People mather) {
+        this.mather = mather;
+    }
+
+    public void setFather(People father) {
+        this.father = father;
+    }
+
+    public void addChildren(People child) {
+       this.children.add(child);
+    }
+    
+    public List<People> getChildren() {
+        return children;
+    }
+
+    public People getMather() {
+        return mather;
+    }
+
+    public People getFather() {
+        return father;
+    }
 
 }
