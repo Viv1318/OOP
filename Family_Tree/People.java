42acd3dd5b823af11fc92ebf27727cb45bb24c71
oleg voronin <oleg83voronin@gmail.com>
2024-10-23 20:04:48 +0300

    add atributs

--- Family_Tree/People.java
+++ Family_Tree/People.java
@@ -1,8 +1,16 @@
 package Family_Tree;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class People {
-    
 
+    String name;
+    LocalDate birthDate;
+    String mather;
+    String father;
+    ArrayList<String> children;
 
-    
+    public 
+
 }
