c72142cc6c33faf74ab8561e864ad4b06c2249cb
oleg voronin <oleg83voronin@gmail.com>
2024-10-23 22:35:42 +0300

    change file

--- Family_Tree/Main.java
+++ Family_Tree/Main.java
@@ -3,7 +3,12 @@ package Family_Tree;
 public class Main {
     
     public static void main (String[] args ){
-        
+
+        FamalyTree famalyTree = new FamalyTree ();
+
+        People Voronin_N_A = new People ("Воронин Н.А.", 1960);
+        People Voronin_L_D = new People ("Воронина Л.Д.", 1965);
+
     }
 
 }
